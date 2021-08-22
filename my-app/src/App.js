5bd0250aab9317a98f2857826b923ed0040dadb1
Karthik V <kartikvitla@gmail.com>
2021-08-22 10:32:28 +0530

    Commit the changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+// import React from 'react'
+
 import './App.css';
 import ClassCounter from './Component/ClassCounter';
 import HookCounter from './Component/HookCounter.js';
@@ -9,11 +11,34 @@ import HookCounterOnne from './Component/HookCounterOnne';
 import ClassCounterOne from './Component/ClassCounterOne';
 import HookMouse from './Component/HookMouse';
 import Datafetching from './Component/Datafetching';
+import Counter1 from './Component/Counter1';
+import Counter2 from './Component/Counter2';
+import Counterr from './Component/Counterr';
+import DocTitleOne from './DocTitleOne';
+import CounterrrOne from './Component/CounterrrOne';
+import Counterrr2 from './Component/Counterrr2';
+// import ComponentC from './Component/ComponentC'
+
+// export const UserContext = React.createContext() // context
+// export const ChannelContext = React.createContext()
+
 
 function App() {
+  
   return (
     <div className="App">
-      <Datafetching />
+      <CounterrrOne />
+      <Counterrr2 />
+      {/* <DocTitleOne /> */}
+      {/* <Counterr /> */}
+      {/* <Counter2 /> */}
+            {/* <Counter1 /> * /}
+			{/* <UserContext.Provider value={'Vishwas'}>
+        <ChannelContext.Provider value={'Codevolution'}> //context
+					<ComponentC />
+				</ChannelContext.Provider>
+			</UserContext.Provider> */}
+      {/* <Datafetching /> */}
       {/* <ClassCounterOne /> */}
       {/* <HookMouse /> */}
       {/* <HookCounterOnne /> */}
